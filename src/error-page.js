75432a8fc87899f68cde29e4311b5194c539e670
Edward Nawrocki <eddnawrocki@gmail.com>
2023-03-03 18:45:26 -0800

    Moved home page js to Home.js, created NewItem page, errorPage and ReactRouter

---
+++ src/error-page.js
@@ -0,0 +1,14 @@
+import React from "react";
+import { useRouteError } from "react-router-dom";
+
+export default function ErrorPage() {
+    const error = useRouteError();
+    console.error(error);
+
+    return (
+        <div id="error-page">
+            <h1>Oops!</h1>
+            <p>An unexpected error has occured</p>
+        </div>
+    );
+}
---
+++ src/routes/NewItem.js
@@ -0,0 +1,10 @@
+import React from "react";
+import { useEffect, useState } from 'react';
+import styled from 'styled-components';
+import 'font-awesome/css/font-awesome.min.css';
+
+export default function NewItem() {
+    return (
+        <h1>HEADER</h1>
+    );
+}
