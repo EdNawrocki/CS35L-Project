9530db552838c33332839c51b4cda66c941d6a48
Edward Nawrocki <eddnawrocki@gmail.com>
2023-03-05 15:10:03 -0800

    Integrated Luke's firebase backend with the old frontend

--- README.md
+++ README.md
@@ -1,6 +1,3 @@
-
-# CS35L-Project
-Final Project for CS35L 
 # Getting Started with Create React App
 
 This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
--- src/App.js
+++ src/App.js
@@ -1,45 +1,45 @@
-import { useContext } from "react"
-import { BrowserRouter as Router, Routes, Route, Link, Navigate } from "react-router-dom"
+import {useContext} from "react"
+import {BrowserRouter as Router, Routes, Route, Link, Navigate} from "react-router-dom"
 import Home from "./pages/home"
 import Login from "./pages/login"
 import NewItem from "./pages/newitem"
 import { AuthContext } from "./context/AuthContext"
 
 function App() {
-    const { currentUser } = useContext(AuthContext)
+  const {currentUser} = useContext(AuthContext)
 
-    const RequireAuth = ({ children }) => {
-        return currentUser ? (children) : <Navigate to="/login" />
-    }
+  const RequireAuth = ({children}) => {
+    return currentUser ? (children) : <Navigate to="/login" />
+  }
 
-    return (
-        <Router>
-            <nav>
-                <Link to="/">Home</Link>
-                <Link to="/login">Login</Link>
-                <Link to="/newitem">NewItem</Link>
-            </nav>
-            <Routes>
-                <Route path="/login" element={<Login />} />
-                <Route
-                    path="/"
-                    element={
-                        <RequireAuth>
-                            <Home />
-                        </RequireAuth>
-                    }
-                />
-                <Route
-                    path="/newitem"
-                    element={
-                        <RequireAuth>
-                            <NewItem />
-                        </RequireAuth>
-                    }
-                />
-            </Routes>
-        </Router>
-    );
+  return (
+    <Router>
+      <nav>
+        <Link to="/">Home</Link>
+        <Link to="/login">Login</Link>
+        <Link to="/newitem">NewItem</Link>
+      </nav>
+      <Routes>
+        <Route path="/login" element={<Login />} />
+        <Route
+          path="/"
+          element={
+            <RequireAuth>
+              <Home />
+            </RequireAuth>
+          }
+        />
+        <Route
+          path="/newitem"
+          element={
+            <RequireAuth>
+              <NewItem />
+            </RequireAuth>
+          }
+        />
+      </Routes>
+    </Router>
+  );
 }
 
 export default App;
