af819813da6622aef34cf70b09edde34fea882da
Luke Westmark <lukewestmark@g.ucla.edu>
2023-03-08 15:30:39 -0800

    Add offline data generation
    * We run into issue with database fetch limits; address this by
      allowing offline generation of random data to test/develop
    * FIX bug causing looped fetches from Firestore database

--- src/pages/viewitem.js
+++ src/pages/viewitem.js
@@ -3,25 +3,36 @@ import { collection, getDocs } from "firebase/firestore";
 import { db } from "../firebase";
 
 function ViewItem() {
+    const PULL_FROM_DB = false;            // set true for data base pulling, DELETE FOR FINAL PRODUCT
     const itemsRef = collection(db, "items");
 
     const [itemList, setItemList] = useState([]);
 
     useEffect(() => {
+                                        if (!PULL_FROM_DB) { return; }  // DELETE FOR FINAL PRODUCT
         const getItems = async () => {
             const data = await getDocs(itemsRef);
             setItemList(data.docs.map((doc) => ({ ...doc.data(), id: doc.id })));
         };
 
         getItems();
-    })
+    }, [])
+
+    if (! PULL_FROM_DB && itemList.length === 0) {      // DELETE WHOLE IF STATEMENT FOR FINAL PRODUCT
+        const fixedList = [];
+        for (let i = 0; i != 50; i++) {
+            fixedList.push({ name: "test" + i.toString(), quantity: Math.floor(Math.random() * 10000), id: i })
+        }
+        setItemList(fixedList);
+    }
 
     return(
         <div>
             <table>
                 {itemList.map((item) => {
+                    console.log(item);
                     return (
-                        <tr>
+                        <tr key={item.id}>
                             <td>{item.name}</td>
                             <td>{item.quantity}</td>
                         </tr>
