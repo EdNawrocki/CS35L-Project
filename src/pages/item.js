a597de06ab37793fb5e8b2a7d78cacbca575d0ca
Albert Guan <albertguan@ucla.edu>
2023-03-10 16:50:35 -0800

    basic graph structure

--- src/pages/item.js
+++ src/pages/item.js
@@ -1,6 +1,8 @@
 import { useLocation, useNavigate } from "react-router-dom"
 import { getFirestore, doc, deleteDoc } from "firebase/firestore"
-import { useState } from "react"
+import { useState ,React } from "react"
+import Chart from 'chart.js/auto'
+import Plot from 'react-plotly.js'
 
 function Item() {
     // list of all pertinent objects is imported as: 
@@ -25,10 +27,26 @@ function Item() {
 
     const [error, setError] = useState(false);      // state for error in deleting items
 
+    const quantityList = [];
     let total = 0;                                      // tally the total quantity
+    let count =0;
     location.state.itemHistory.forEach((item) => {
         total += Number(item.quantity);
+        quantityList.push(total);
+        count += 1;
     })
+
+    function makeArr(arrSize){
+        var arr = [];
+        //var step = (stopVal - startVal) / (index -1);
+        for (var i = 0; i < arrSize; i++){
+            arr.push(i);
+        }
+        return arr;
+    }
+
+    const xAxis = makeArr(count);
+    
 
     const handleDelete = () => {                            // on clicking 'stop tracking': 
         location.state.itemHistory.forEach((item) => {      // for each document of 'item'
@@ -53,8 +71,9 @@ function Item() {
         title = location.state.item.user;
     }
 
+    
     return(
-        <div>
+        <>
             <h1>{title}'s History:</h1>
             {location.state.itemHistory.map((item) => {         // return a list of item names and quantities
                 return (
@@ -64,8 +83,24 @@ function Item() {
             <p>{location.state.isItemSearch && total}</p>
             {location.state.isItemSearch && <button onClick={handleDelete}>Stop Tracking Item {location.state.item.name}</button>}
             {error && <span>ERROR DELETING</span>}
-        </div>
+            <Plot
+            data= {[
+                {
+                x: xAxis,
+                y: quantityList,
+                type: 'scatter',
+                }]
+            }
+            layout={ {width: 320, height: 240, title: "Graph"}}/>
+            
+        </>
+        
     );
 }
+
+
+
+
+
 
 export default Item;
