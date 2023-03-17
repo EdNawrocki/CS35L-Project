413948f798f69eabde00245bce0182dc7e34d351
Albert Guan <albertguan@ucla.edu>
2023-03-17 13:28:39 -0700

    bug fix: don't update databse when quantiy or name is empty

--- src/pages/newitem.js
+++ src/pages/newitem.js
@@ -9,15 +9,24 @@ function NewItem() {
     const [name, setName] = useState("")                // state for item name input
     const [quantity, setQuantity] = useState(0)         // state for item quantity input
 
+    const [error, setError] = useState(false)
+
     const handleAdd = async(e) =>{              // upon clicking 'Add Item'...
         e.preventDefault()                          // make sure something was inputted
-
+        setError(false)
+        if(name.length == 0 || quantity ==0)
+        {
+            setError(true)
+        }
+        else{   
         await addDoc(collection(db, "items"), {     // add that item to the DB with properties: 
             name: name,                                 // name of item
             quantity: quantity,                         // quantity of item
             user: currentUser.email,                    // user email of item adder
             timeStamp: serverTimestamp(),               // time added
-        });
+        });}
+        setName("");
+        setQuantity(0);
         e.target.reset();
     }
 
@@ -29,7 +38,8 @@ function NewItem() {
                 <div className="FormWrapper">
                 <input className="newItem" type="name" placeholder="Item Name" onChange={e=>setName(e.target.value)} />
                 <input className="newItem" type="quantity" placeholder="Quantity" onChange={e=>setQuantity(e.target.value)} />
-                    <button className="newItem" type="submit">Submit</button>
+                <button className="newItem" type="submit">Submit</button>
+                
                 </div>
                 </form>
             
